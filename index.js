0d8767ac765e615488d45484f4e6fd25a9e7de48
Trin Pongsri <trin.pongsri@gmail.com>
2023-10-02 00:20:39 +0700

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 let mybutton = document.getElementById("topBtn");
 
@@ -15,17 +15,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
-
-var images = [
-  'https://www.reverautomotive.com/_next/image?url=%2Fimages%2Fmodel%2Fseal%2Fcolor%2Fseal-horizon-white.png&w=1920&q=100',
-  'https://www.reverautomotive.com/_next/image?url=%2Fimages%2Fmodel%2Fseal%2Fcolor%2Fseal-quantum-black.png&w=1920&q=100',
-  'https://www.reverautomotive.com/_next/image?url=%2Fimages%2Fmodel%2Fseal%2Fcolor%2Fseal-velocity-blue.png&w=1920&q=100',
-  'https://www.reverautomotive.com/_next/image?url=%2Fimages%2Fmodel%2Fseal%2Fcolor%2Fseal-space-grey.png&w=1920&q=100'
-];
-
-var image = document.getElementById('image');
-
-function changeImage(index) {
-  image.src = images[index];
 }
